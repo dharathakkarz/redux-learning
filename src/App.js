1dd11c4a5f7a340555f17a09e950617036ed4f28
dharathakkarz <dharat@zignuts.com>
2024-03-08 12:30:45 +0530

    hoc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import {useDispatch, useSelector} from "react-redux"
 import { fetchTodos } from "./redux/slice/todo";
 import './App.css';
+import UserList from "./UserList";
+import TodoList from "./TodoList";
+import SearchUsers from "./UserList";
+import SearchTodos from "./TodoList";
 
 function App() {
 
-  const dispatch = useDispatch();
-  const state = useSelector((state) => state);
-  console.log("state", state)
+  // const dispatch = useDispatch();
+  // const state = useSelector((state) => state);
+  // console.log("state", state)
 
-  if(state.todo.isLoading){
-    return <h1>Loading..</h1>
-  }
+  // if(state.todo.isLoading){
+  //   return <h1>Loading..</h1>
+  // }
 
   return (
     <>
-    <div className= "container">
+    {/* <div className= "container">
     <button onClick={e => dispatch(fetchTodos())} >fetch Todos</button>
     {
      state.todo.data && state.todo.data.map(e => <li>{e.title}</li>)
     }
-    </div>
+    </div> */}
+
+<div>
+  <h2>Higher Order Component</h2>
+  <div className="section">
+    <SearchUsers/>
+  </div>
+  <div className="section">
+    <SearchTodos/>
+  </div>
+</div>
     </>
   );
 }
